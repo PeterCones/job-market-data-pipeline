c148dbc22c983874aac4b44547c2d65fe63aab6a
PeterCones <olly.john@live.co.uk>
2026-03-12 08:36:21 +0000

    Update .gitignore to include necessary directories and remove obsolete entries; fix SQL join reference in skill_demand.sql

--- transforms/models/analytics/skill_demand.sql
+++ transforms/models/analytics/skill_demand.sql
@@ -5,7 +5,7 @@ SELECT
     s.skill_name,
     COUNT(*) AS job_count
 FROM {{ ref('stg_job_skills') }} js
-JOIN staging.skills s ON s.skill_id = js.skill_id
+JOIN {{ref('skills')}} s ON s.skill_id = js.skill_id
 JOIN {{ ref('stg_jobs') }} j ON j.job_id = js.job_id
 GROUP BY s.skill_name
 ORDER BY job_count DESC
